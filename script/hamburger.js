12b21e31e61853fd2f52df1e7287a26c16e74d55
mattmaxtane1203 <mattmaxtane1203@gmail.com>
2022-05-21 10:57:16 +0700

    Added responsive navbar; Added footer; Finished home page;

---
+++ script/hamburger.js
@@ -0,0 +1,18 @@
+function hamburgerActive(){
+    const button = document.getElementById('hamburger');
+    const sidebar = document.getElementById('sidebar');
+    button.classList.toggle('is-active');
+    console.log(sidebar.style.display);
+    if(sidebar.style.display == 'none' || sidebar.style.display == ''){
+        sidebar.style.display = 'flex';
+    } else{
+        sidebar.style.display = 'none';
+    }
+}
+
+function closeSidebar(){
+    const sidebar = document.getElementById('sidebar');
+    sidebar.style.display = 'none';
+}
+
+window.onresize = closeSidebar;
---
+++ script/redirect.js
@@ -0,0 +1,19 @@
+function home(){
+    window.open("../html/index.html", "_self");
+}
+
+function marineLife(){
+    window.open("../html/marineLife.html", "_self");
+}
+
+function marineAnimals(){
+    window.open("../html/marineAnimals.html", "_self");
+}
+
+function aboutUs(){
+    window.open("../html/aboutUs.html", "_self");
+}
+
+function contactUs(){
+    window.open("../html/marineLife.html", "_self");
+}
